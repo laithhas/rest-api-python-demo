748ea0475c74f3b65d1d6fd1de45685620cb2532
Laith <lhasanian@gmail.com>
2022-09-15 16:39:38 -0500

    Hiding API key

--- rest-api-demo.py
+++ rest-api-demo.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
 NYT_ARTICLE_SEARCH_API_BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
 NYT_ARTICLE_SEARCH_API_KEY = 'zBUKdYxLkvd8g0A8cZpgUxz1Z8bNapkj'
@@ -8,7 +12,7 @@ response = requests.get(
     NYT_ARTICLE_SEARCH_API_BASE_URL,
      params={
         'q': 'Texas State University',
-        'api-key': NYT_ARTICLE_SEARCH_API_KEY
+        'api-key': os.getenv('NYT_ARTICLE_SEARCH_API_KEY')
      }
 )
 
@@ -16,7 +20,7 @@ json_data = response.json()
 pretty_json_data = json.dumps(json_data, indent=4, sort_keys=True)
 print(pretty_json_data)
 
-article_objects = json_data["response"]["docs"]
+article_objects = json_data['response']['docs']
 
 for article in article_objects:
-    print(article["headline"]["main"])
+    print(article['headline']['main'])
