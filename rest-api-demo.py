a5977d188c8b8850999d14cfe6f1260ed4a27500
Laith <lhasanian@gmail.com>
2022-09-15 18:21:31 -0500

    woohoo

--- rest-api-demo.py
+++ rest-api-demo.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
 NYT_ARTICLE_SEARCH_API_BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
-NYT_ARTICLE_SEARCH_API_KEY = 'zBUKdYxLkvd8g0A8cZpgUxz1Z8bNapkj'
 
 response = requests.get(
     NYT_ARTICLE_SEARCH_API_BASE_URL,
