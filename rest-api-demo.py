092e579452fc44cc5f91fb03f7ee0407ce7d5778
Laith <lhasanian@gmail.com>
2023-02-23 17:11:41 -0600

    made call to nyt api to get txst articles and hid api key

--- rest-api-demo.py
+++ rest-api-demo.py
@@ -1,27 +1,25 @@
 import requests
 import json
-import os
+from os import getenv
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())  # This is to load your API keys from .env
+load_dotenv(find_dotenv())  # This loads our .env file in this path
 
-NYT_ARTICLE_SEARCH_API_BASE_URL = (
-    "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-)
+NYT_API_BASE_URL = "https://api.nytimes.com/svc/"
+NYT_ARTICLE_SEARCH_PATH = "search/v2/articlesearch.json"
+TXST_SEARCH_QUERY_PARAM = "Texas State University"
+COCAINE_BEAR_QUERY_PARAM = "Cocaine Bear"
 
-response = requests.get(
-    NYT_ARTICLE_SEARCH_API_BASE_URL,
+nyt_rest_api_response = requests.get(
+    NYT_API_BASE_URL + NYT_ARTICLE_SEARCH_PATH,
     params={
-        "q": "Texas State University",
-        "api-key": os.getenv("NYT_ARTICLE_SEARCH_API_KEY"),
+        "q": TXST_SEARCH_QUERY_PARAM,
+        "api-key": getenv("NYT_ARTICLE_SEARCH_API_KEY"),
     },
 )
 
-json_data = response.json()
-pretty_json_data = json.dumps(json_data, indent=4, sort_keys=True)
-print(pretty_json_data)
+pretty_json_data = json.dumps(nyt_rest_api_response.json(), indent=4, sort_keys=True)
+articles_list = nyt_rest_api_response.json()["response"]["docs"]
 
-article_objects = json_data["response"]["docs"]
-
-for article in article_objects:
-    print(article["headline"]["main"])
+for i in range(10):
+    print(articles_list[i]["headline"]["main"])
